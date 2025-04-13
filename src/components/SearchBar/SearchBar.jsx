f8b93f5da8626d9f30c3e71fc439da8c2d3e4970
Tinadin <pitynia8717@gmail.com>
2025-04-14 01:40:34 +0300

    add vanta.js (ref)  and update code & styles

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,39 +1,72 @@
+import { useEffect, useRef, useState } from "react";
+import * as THREE from "three";
+import WAVES from "vanta/dist/vanta.globe.min";
+
 import toast from "react-hot-toast";
 import { Field, Form, Formik } from "formik";
 import { TbPhotoSearch } from "react-icons/tb";
-
 import css from "./SearchBar.module.css";
 
 const initialValues = { query: "" };
 
 const SearchBar = ({ onSubmit }) => {
+  const vantaRef = useRef(null);
+  const [vantaEffect, setVantaEffect] = useState(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        WAVES({
+          el: vantaRef.current,
+          THREE: THREE,
+          color2: 0xa8ed7,
+          color: 0x891ec8,
+          backgroundColor: 0x1c1c37,
+          size: 1.3,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1,
+          scaleMobile: 1.0,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
   return (
-    <header className={css.header}>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={(values, actions) => {
-          if (!values.query) {
-            toast.error("Please enter the value in the search field");
-            return;
-          }
-          onSubmit(values.query);
-          actions.resetForm();
-        }}
-      >
-        <Form className={css.form}>
-          <Field
-            className={css.input}
-            type="search"
-            name="query"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-          <button className={css.btnSearch} type="submit">
-            <TbPhotoSearch className={css.icon} />
-          </button>
-        </Form>
-      </Formik>
+    <header className={`${css.header}`} ref={vantaRef}>
+      <div className={css.formWrapper}>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={(values, actions) => {
+            if (!values.query) {
+              toast.error("Please enter the value in the search field");
+              return;
+            }
+            onSubmit(values.query);
+            actions.resetForm();
+          }}
+        >
+          <Form className={css.form}>
+            <Field
+              className={css.input}
+              type="search"
+              name="query"
+              autoComplete="off"
+              autoFocus
+              placeholder="Search images and photos"
+            />
+            <button className={css.btnSearch} type="submit">
+              <TbPhotoSearch className={css.icon} />
+            </button>
+          </Form>
+        </Formik>
+      </div>
     </header>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,33 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     sourcemap: true,
+//   }
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
-  }
+  },
+  optimizeDeps: {
+    exclude: [
+      'three-mesh-bvh',
+      'three/addons/renderers/webgl/nodes/WebGLNodes.js',
+      'three-subdivide',
+      'web-ifc-three',
+      'web-ifc',
+      'three-bvh-csg',
+      'three-gpu-pathtracer',
+      'flow',
+      'three/addons/loaders/IFCLoader.js',
+    ],
+  },
 });
